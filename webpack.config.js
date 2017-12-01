dd1d9492d2f18796c6ee2376e8eba1cd87a44235
‘Tim <timothy.j.ocampo@gmail.com>
2017-11-30 17:53:31 -0800

    Set up HTML Plugin for webpack -created index.html template --> tried to set up minify feature but, only can minify app.bundle.js
    Review this later and get minify feature to work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports ={
+
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+
+        path: __dirname + "./dist",
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+
+        new HtmlWebpackPlugin({
+            title: 'Project Template',
+            minify:{collapseWhiteSpace: true},
+            hash: true,
+            template: './src/ejs/index.ejs',
+        })
+    ]
 };
